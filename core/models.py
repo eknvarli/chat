3e98e2e283122351d3916d2d6b372759a027c274
Ekin İlter Varlı <ekinnos@tutanota.com>
2024-01-14 14:50:26 +0300

    Updated GitIgnore

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    avatar = models.ImageField(upload_to='media/avatars/', blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
     # ...
 
     class Meta:
